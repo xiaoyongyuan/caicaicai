72249fb72b6833d06f4dd5565a7cd7bc8e4c0279
xiaoyongyuan <18710962446@sina.cn>
2020-09-28 18:11:45 +0800

    音乐暂停的修改

--- js/Questions.js
+++ js/Questions.js
@@ -3,75 +3,224 @@
  */
 
 var QuestionJosn = [{
-    "questionId": "1",
-    "questionType": "img",
-    "questionSource": "./source/img/1.jpg",
-    "questionTitle": "黄土高原特有的传统民居是？",
-    "questionItems": "窑洞;竹楼;蒙古包;四合院",
-    "questionAnswer": "A"
-}, {
-    "questionId": "2",
-    "questionType": "img",
-    "questionSource": "./source/img/2.jpg",
-    "questionTitle": "下面文化中，属于黄土高原地区的是？",
-    "questionItems": "粤西木偶;浙江嵊县竹编;滦州皮影;陕北腰鼓",
-    "questionAnswer": "D"
-}, {
-    "questionId": "3",
-    "questionType": "img",
-    "questionSource": "./source/img/3.jpg",
-    "questionTitle": "下列属于陕北民歌的是？",
-    "questionItems": "山曲;白曲;信天游;川江号子",
-    "questionAnswer": "C"
-}, {
-    "questionId": "4",
-    "questionType": "img",
-    "questionSource": "./source/img/4.jpg",
-    "questionTitle": "以下哪个是陕北著名的革命纪念地？",
-    "questionItems": "沂蒙革命遗址;井冈山革命遗址;延安革命纪念馆;香山革命纪念地",
-    "questionAnswer": "C"
-}, {
-    "questionId": "5",
-    "questionType": "img",
-    "questionSource": "./source/img/5.jpg",
-    "questionTitle": "下面文化中，属于黄土高原地区的是？",
-    "questionItems": "河北蔚县染色刻纸;陕北说书;广东金漆木雕;苏州宋锦",
-    "questionAnswer": "B"
-}, {
-    "questionId": "6",
-    "questionType": "img",
-    "questionSource": "./source/img/6.jpg",
-    "questionTitle": "下面哪个是陕北著名的地貌情形？",
-    "questionItems": "青藏高原;黄土高原;松辽盆地;长江中下游平原",
-    "questionAnswer": "B"
-}, {
-    "questionId": "7",
-    "questionType": "img",
-    "questionSource": "./source/img/7.jpg",
-    "questionTitle": "以下哪条河流是流经黄土高原的？",
-    "questionItems": "闽江;珠江;长江;黄河",
-    "questionAnswer": "D"
-}, {
-    "questionId": "8",
-    "questionType": "img",
-    "questionSource": "./source/img/8.jpg",
-    "questionTitle": "《诗经》曾云：“古公亶父，陶复陶穴，未有家室。”中指的是？",
-    "questionItems": "窑洞;竹楼;土房;瓦房",
-    "questionAnswer": "A"
-}, {
-    "questionId": "9",
-    "questionType": "img",
-    "questionSource": "./source/img/9.jpg",
-    "questionTitle": "下面哪个属于陕北腰鼓文化？",
-    "questionItems": "十番箫鼓;安塞腰鼓;潮州苏锣鼓;东北腰鼓",
-    "questionAnswer": "B"
-}, {
-    "questionId": "10",
-    "questionType": "img",
-    "questionSource": "./source/img/10.jpg",
-    "questionTitle": "以下哪首歌是陕北民歌？",
-    "questionItems": " 《弥渡山歌》;《兰花花》;《二十四孝歌》;《蝴蝶泉边》",
-    "questionAnswer": "B"
+"questionId":"1",
+"questionType":"img",
+"questionSource":"./source/img/1.jpg",
+"questionTitle":"黄土高原特有的传统民居是？",
+"questionItems":"窑洞;竹楼;蒙古包;四合院",
+"questionAnswer":"A"
+},{
+"questionId":"2",
+"questionType":"img",
+"questionSource":"./source/img/2.jpg",
+"questionTitle":"下面文化中，属于黄土高原地区的是？",
+"questionItems":"粤西木偶;浙江嵊县竹编;滦州皮影;陕北腰鼓",
+"questionAnswer":"D"
+},{
+"questionId":"3",
+"questionType":"img",
+"questionSource":"./source/img/3.jpg",
+"questionTitle":"下列属于陕北民歌的是？",
+"questionItems":"山曲;白曲;信天游;川江号子",
+"questionAnswer":"C"
+},{
+"questionId":"4",
+"questionType":"img",
+"questionSource":"./source/img/4.jpg",
+"questionTitle":"以下哪个是陕北著名的革命纪念地？",
+"questionItems":"沂蒙革命遗址;井冈山革命遗址;延安革命纪念馆;香山革命纪念地",
+"questionAnswer":"C"
+},{
+"questionId":"5",
+"questionType":"img",
+"questionSource":"./source/img/5.jpg",
+"questionTitle":"下面文化中，属于黄土高原地区的是？",
+"questionItems":"河北蔚县染色刻纸;陕北说书;广东金漆木雕;苏州宋锦",
+"questionAnswer":"B"
+},{
+"questionId":"6",
+"questionType":"img",
+"questionSource":"./source/img/6.jpg",
+"questionTitle":"下面哪个是陕北著名的地貌情形？",
+"questionItems":"青藏高原;黄土高原;松辽盆地;长江中下游平原",
+"questionAnswer":"B"
+},{
+"questionId":"7",
+"questionType":"img",
+"questionSource":"./source/img/7.jpg",
+"questionTitle":"以下哪条河流是流经黄土高原的？",
+"questionItems":"闽江;珠江;长江;黄河",
+"questionAnswer":"D"
+},{
+"questionId":"8",
+"questionType":"img",
+"questionSource":"./source/img/8.jpg",
+"questionTitle":"《诗经》曾云：“古公亶父，陶复陶穴，未有家室。”中指的是？",
+"questionItems":"窑洞;竹楼;土房;瓦房",
+"questionAnswer":"A"
+},{
+"questionId":"9",
+"questionType":"img",
+"questionSource":"./source/img/9.jpg",
+"questionTitle":"下面哪个属于陕北腰鼓文化？",
+"questionItems":"十番箫鼓;安塞腰鼓;潮州苏锣鼓;东北腰鼓",
+"questionAnswer":"B"
+},{
+"questionId":"10",
+"questionType":"img",
+"questionSource":"./source/img/10.jpg",
+"questionTitle":"以下哪首歌是陕北民歌？",
+"questionItems":" 《弥渡山歌》;《兰花花》;《二十四孝歌》;《蝴蝶泉边》",
+"questionAnswer":"B"
+},{
+"questionId":"11",
+"questionType":"mp3",
+"questionSource":"./source/mp3/1.mp3",
+"questionTitle":"下面歌曲的名字是？",
+"questionItems":"蓝花花;竹楼;女孩担水;四合院",
+"questionAnswer":"A"
+},{
+"questionId":"12",
+"questionType":"mp3",
+"questionSource":"./source/mp3/2.mp3",
+"questionTitle":"下面歌曲的名字是？",
+"questionItems":"对花;五哥放羊;放风筝;九连环",
+"questionAnswer":"D"
+},{
+"questionId":"13",
+"questionType":"mp3",
+"questionSource":"./source/mp3/3.mp3",
+"questionTitle":"下面歌曲的名字是？",
+"questionItems":"十里墩;九连环;打樱桃;大拜年",
+"questionAnswer":"C"
+},{
+"questionId":"14",
+"questionType":"mp3",
+"questionSource":"./source/mp3/4.mp3",
+"questionTitle":"下面歌曲的名字是？",
+"questionItems":"小放牛;东方红;五哥放羊;天心顺",
+"questionAnswer":"A"
+},{
+"questionId":"15",
+"questionType":"mp3",
+"questionSource":"./source/mp3/5.mp3",
+"questionTitle":"下面歌曲的名字是？",
+"questionItems":"九连环;天心顺;东方红;寄生草",
+"questionAnswer":"B"
+},{
+"questionId":"16",
+"questionType":"mp3",
+"questionSource":"./source/mp3/6.mp3",
+"questionTitle":"下面歌曲的名字是？",
+"questionItems":"青藏高原;二月里来;生产大合唱;军民大生产",
+"questionAnswer":"B"
+},{
+"questionId":"17",
+"questionType":"mp3",
+"questionSource":"./source/mp3/7.mp3",
+"questionTitle":"下面歌曲的名字是？",
+"questionItems":"东方红;二月里来;有吃有穿;走西口",
+"questionAnswer":"D"
+},{
+"questionId":"18",
+"questionType":"mp3",
+"questionSource":"./source/mp3/8.mp3",
+"questionTitle":"下面歌曲的名字是？",
+"questionItems":"信天游;想你哩;走西口;酒曲",
+"questionAnswer":"A"
+},{
+"questionId":"19",
+"questionType":"mp3",
+"questionSource":"./source/mp3/9.mp3",
+"questionTitle":"下面歌曲的名字是？",
+"questionItems":"走西口;黄土高坡;信天游;女儿歌",
+"questionAnswer":"B"
+},{
+"questionId":"20",
+"questionType":"mp3",
+"questionSource":"./source/mp3/10.mp3",
+"questionTitle":"下面歌曲的名字是？",
+"questionItems":" 弥渡山歌;我的陕北;二十四孝歌;蝴蝶泉边",
+"questionAnswer":"B"
+},{
+"questionId":"21",
+"questionType":"video",
+"questionSource":"./source/video/郭兰英《咱们的领袖毛泽东》豪迈雄壮 无限深情(超清).mp4",
+"questionTitle":"这首歌曲歌颂的是哪一位伟大领袖？",
+"questionItems":"孙中山;毛泽东;周恩来;邓小平",
+"questionAnswer":"b"
+},
+{
+"questionId":"22",
+"questionType":"video",
+"questionSource":"./source/video/《保卫黄河》经典再现，音乐响起的那一刻，顿觉浑身充满力量！(超清).mp4",
+"questionTitle":"视频播放的《保卫黄河》是《黄河大合唱》的第七乐章，写成于中国那一时期？",
+"questionItems":"抗日战争时期;抗美援朝时期;改革开放时期;中国特色社会主义新时代",
+"questionAnswer":"a"
+},
+{
+"questionId":"23",
+"questionType":"video",
+"questionSource":"./source/video/郭兰英-兰花花 (KTV版)(高清).mp4",
+"questionTitle":"视频播放的歌曲名称是？",
+"questionItems":"《黄河船夫曲》;《东方红》;《挂红灯》;《兰花花》",
+"questionAnswer":"d"
+},
+{
+"questionId":"24",
+"questionType":"video",
+"questionSource":"./source/video/李政飞-吉祥腰鼓 (KTV版)(高清).mp4",
+"questionTitle":"歌曲《吉祥腰鼓》视频中展示的是哪一种传统民俗舞蹈？",
+"questionItems":"苗族鼓舞;太平鼓舞;安塞腰鼓;花钹大鼓",
+"questionAnswer":"c"
+},
+{
+"questionId":"25",
+"questionType":"video",
+"questionSource":"./source/video/一曲《东方红》歌颂伟大的领袖，缅怀先烈，致敬中国军人！(超清).mp4",
+"questionTitle":"播放中的民歌《东方红》曲调原为哪一首陕北民歌？",
+"questionItems":"《骑白马》;《跑旱船》;《大拜年》;《小放牛》",
+"questionAnswer":"a"
+},
+{
+"questionId":"26",
+"questionType":"video",
+"questionSource":"./source/video/老电影《人生》插曲《叫一声哥哥你快回来》冯健雪(超清).mp4",
+"questionTitle":"视频播放《叫一声哥哥你快回来》是哪一部电影的插曲",
+"questionItems":"《红高粱》;《海霞》;《人生》;《白毛女》",
+"questionAnswer":"c"
+},
+{
+"questionId":"27",
+"questionType":"video",
+"questionSource":"./source/video/王向荣唱的《黄河船夫曲》就是好听！(超清).mp4",
+"questionTitle":"下列哪一项是陕北民歌《黄河船夫曲》的曲调特点",
+"questionItems":"粗犷高昂;悠扬舒缓;欢快活跃;忧郁低沉",
+"questionAnswer":"a"
+},
+{
+"questionId":"28",
+"questionType":"video",
+"questionSource":"./source/video/杨紫萍-南泥湾 (KTV版)(高清).mp4",
+"questionTitle":"视频播放的陕北民歌《南泥湾》曲旋律优美、抒情，热情歌颂了开荒生产建立功勋的（  ）",
+"questionItems":"红军战士;解放军战士;新四军战士;八路军战士",
+"questionAnswer":"d"
+},
+{
+"questionId":"29",
+"questionType":"video",
+"questionSource":"./source/video/万山红-山丹丹开花红艳艳 (KTV版)(高清).mp4",
+"questionTitle":"视频播放的陕北民歌《山丹丹开花红艳艳》描绘了中国革命重心从（   ），延安成为革命的圣地的历史",
+"questionItems":"北方移到南方;南方移到北方;东方移到西方;西方移到东方",
+"questionAnswer":"b"
+},
+{
+"questionId":"30",
+"questionType":"video",
+"questionSource":"./source/video/佚名-翻身道情 (KTV版)(高清).mp4",
+"questionTitle":"《翻身道情》描绘的是（  ）的故事",
+"questionItems":"穷苦人民;军队战士;地主土豪;知识分子",
+"questionAnswer":"a"
 }
 ];
 var luyouqi = [{
